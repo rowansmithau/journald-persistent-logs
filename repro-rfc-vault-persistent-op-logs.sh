5d02d45f4518c4135007f587955fa192d51f4d6b
Rowan Smith <rowan.smith@hashicorp.com>
2023-04-06 13:44:50 +1000

    added apt update command

--- repro-rfc-vault-persistent-op-logs.sh
+++ repro-rfc-vault-persistent-op-logs.sh
@@ -15,6 +15,7 @@ if [ "$(which yum 2>/dev/null)" ]; then
 elif [ "$(which apt 2>/dev/null)" ]; then
   wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg &&
   echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+  sudo apt update
 else
   echo "not rpm or deb based distro, exiting"
 fi
